6fe171061648c9ba927b9e2477a9d97ae13f8cc0
Ivan <Ivan@RUBEN>
2025-05-07 12:09:37 +0200

    Repaso

---
+++ src/Repaso/RepasoAdrianGonzalez.java
@@ -0,0 +1,17 @@
+package Repaso;
+
+public class RepasoAdrianGonzalez {
+	
+	public static void main(String[] args) {
+
+		System.out.println("Hola,\r\n"
+				+ "Soy el abogado del difunto Príncipe nigeriano Obaluwaji III."
+				+ "\nEn su última voluntad te nombró único heredero de su fortuna: 92 millones de dólares."
+				+ "\nSolo necesito tus datos bancarios, tu dirección, y una foto tuya."
+				+ "\nActúa rápido antes de que el gobierno africano reclame el dinero."
+				+ "\nGracias por tu confianza."
+				+ "\nAtentamente,"
+				+ "\nLic. Uche Nwankwo"
+				+ "\nAbogado real de la corte nigeriana");
+	}
+}
