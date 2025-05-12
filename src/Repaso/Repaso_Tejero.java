7adc2fc472b410775f948470632e257f640a4fc6
Usuario <Usuario@DESKTOP-TA74P1I>
2025-05-12 09:08:03 +0200

    Rama Añadida

---
+++ src/Repaso/Repaso_Tejero.java
@@ -0,0 +1,17 @@
+package Repaso;
+
+public class Repaso_Tejero {
+
+	public static void main(String[] args) {
+		System.out.println("Si te diera un beso en la boca… ¿QUÉ HARÍAS?\r\n"
+				+ "\r\n"
+				+ "1.- Me darías un puñetazo.\r\n"
+				+ "2.- Te enojarías conmigo.\r\n"
+				+ "3.- Te irías sin decirme nada.\r\n"
+				+ "4.- Me seguirías besando.\r\n"
+				+ "5.- Me pedirías tener sexo.\r\n"
+				+ "\r\n"
+				+ "¡Manda esta cadena a 10 personas y descubre lo que harían! Queda prohibido volverme a pasar esta cadena a mi.");
+	}
+
+}
